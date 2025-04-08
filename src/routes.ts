34059228659c9fffd92a5c7fd41bd6060e5d345f
Gabriel Amaral <gabrielvieira2205@gmail.com>
2025-04-08 11:52:37 -0300

    Criando a Tabela de Empresas

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express"
 import { candidatesController } from "./controllers/candidates-controller"
+import { companiesController } from "./controllers/companies-controller"
 
 const router = express.Router()
 
@@ -12,5 +13,7 @@ router.get("/candidates/:id", candidatesController.show)
 router.post("/candidates", candidatesController.save)
 router.put("/candidates/:id", candidatesController.update)
 router.delete("/candidates/:id", candidatesController.delete)
+
+router.get("/companies", companiesController.index)
 
 export { router }
