60fa6a8afbafd78ed7f3cda687035422875b4be5
Gabriel Amaral <gabrielvieira2205@gmail.com>
2025-04-09 12:50:54 -0300

    Endpoints de Vagas de Emprego

--- src/controllers/jobs-controller.ts
+++ src/controllers/jobs-controller.ts
@@ -1,11 +1,81 @@
 import { Request, Response } from "express"
 import { Job } from "../models"
-
 export const jobsController = {
   index: async (req: Request, res: Response) => {
     try {
       const jobs = await Job.findAll({ include: "company" })
       res.json(jobs)
+    } catch (err) {
+      if (err instanceof Error) {
+        res.status(400).json({ message: err.message })
+      }
+    }
+  },
+
+  save: async (req: Request, res: Response) => {
+    const { title, description, limitDate, companyId } = req.body
+
+    try {
+      const job = await Job.create({
+        title,
+        description,
+        limitDate,
+        companyId,
+      })
+
+      res.status(201).json(job)
+    } catch (err) {
+      if (err instanceof Error) {
+        res.status(400).json({ message: err.message })
+      }
+    }
+  },
+
+  show: async (req: Request, res: Response) => {
+    const { id } = req.params
+
+    try {
+      const job = await Job.findByPk(id, { include: "company" })
+      res.json(job)
+    } catch (err) {
+      if (err instanceof Error) {
+        res.status(400).json({ message: err.message })
+      }
+    }
+  },
+
+  update: async (req: Request, res: Response) => {
+    const { id } = req.params
+    const { title, description, limitDate, companyId } = req.body
+
+    try {
+      const [affectedRows, jobs] = await Job.update(
+        {
+          title,
+          description,
+          limitDate,
+          companyId,
+        },
+        {
+          where: { id },
+          returning: true,
+        }
+      )
+
+      res.json(jobs[0])
+    } catch (error) {
+      if (error instanceof Error) {
+        res.status(400).json({ message: error.message })
+      }
+    }
+  },
+
+  delete: async (req: Request, res: Response) => {
+    const { id } = req.params
+
+    try {
+      const job = await Job.destroy({ where: { id } })
+      res.status(204).send()
     } catch (err) {
       if (err instanceof Error) {
         res.status(400).json({ message: err.message })
